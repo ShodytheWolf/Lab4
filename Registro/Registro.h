91ff8e31c2ca3aa72c11cff3e4aa5a31dff1bdff
jostelk <jostelk@users.noreply.github.com>
2022-06-13 22:41:20 -0300

    Update Registro.h

--- Registro/Registro.h
+++ Registro/Registro.h
@@ -7,10 +7,18 @@ class Registro
 {
 public:
     Registro();
+    Registro(float cantHoras,enumPuntuacion puntajeJuego );
     ~Registro();
 
+    dtVideoJuego getDtJuego();
+    //set<dtVideoJuego> getDtVideoJuego();
+    void activarSuscripcion(enumSuscripcion tipoSuscripcion, enumPago metodoDePago);
+    void cancelSuscripcion();
+    bool estaSuscrito();
+    void eliminoContratacion();
 private:
-
+    float cantHoras;
+    enumPuntuacion puntajeJuego; 
 };
 
 #endif
