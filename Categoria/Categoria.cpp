d348a58b6558bddd11a4d2657b55645d9034ce65
Braian <braianbarrios_29@hotmail.com>
2022-06-13 21:33:57 -0300

    Cabezales de Categoria hechos

--- Categoria/Categoria.cpp
+++ Categoria/Categoria.cpp
@@ -1,11 +1,37 @@
 #include "Categoria.h"
 
-Categoria::Categoria()
+Categoria::Categoria(char* nombreCategoria, char* descripcionCategoria, enumCategoria tipoCategoria)
 {
-
+    this->nombreCategoria = nombreCategoria;
+    this->descripcionCategoria = descripcionCategoria;
+    this->tipoCategoria = tipoCategoria;
 }
 
 Categoria::~Categoria()
 {
 
+}
+
+char* Categoria::getNombreCategoria() {
+    return this->nombreCategoria;
+}
+
+void Categoria::setNombreCategoria(char* nombreCategoria) {
+    this->nombreCategoria = nombreCategoria;
+}
+
+char* Categoria::getDescripcionCategoria() {
+    return this->descripcionCategoria;
+}
+
+void Categoria::setDescripcionCategoria(char* descripcionCategoria) {
+    this->descripcionCategoria = descripcionCategoria;
+}
+
+enumCategoria Categoria::getTipoCategoria() {
+	return this->tipoCategoria;
+}
+
+void Categoria::setTipoCategoria(enumCategoria tipoCategoria) {
+	this->tipoCategoria = tipoCategoria;
 }
--- Categoria/Categoria.h
+++ Categoria/Categoria.h
@@ -1,17 +1,30 @@
 #ifndef CATEGORIA_H
 #define CATEGORIA_H
 #include "../colecciones-genericas/interfaces/ICollectible.h"
+#include "../Datatypes/dtCategoria.h"
+#include "../Enumeration/enumCategoria.h"
 
 #pragma once
 
 class Categoria : public ICollectible
 {
 public:
-    Categoria();
+    Categoria(char* nombreCategoria, char* descripcionCategoria, enumCategoria tipoCategoria);
     ~Categoria();
+    char* getNombreCategoria();
+    void setNombreCategoria(char* nombreCategoria);
+    char* getDescripcionCategoria();
+    void setDescripcionCategoria(char* descripcionCategoria);
+    enumCategoria getTipoCategoria();
+    void setTipoCategoria(enumCategoria tipoCategoria);
+    dtCategoria getDtGenero();
+    dtCategoria getDtPlataforma();
+    dtCategoria getDtOtro();
 
 private:
-
+    char* nombreCategoria;
+    char* descripcionCategoria;
+    enumCategoria tipoCategoria;
 };
 
 #endif
