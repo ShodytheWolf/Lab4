f9e1a07dc9024d2f2ed5e4a21957674e68d42ac6
Braian <braianbarrios_29@hotmail.com>
2022-06-13 21:08:49 -0300

    Caezales de Patida hecho

--- Partida/Partida.cpp
+++ Partida/Partida.cpp
@@ -5,7 +5,37 @@ Partida::Partida()
 
 }
 
+Partida::Partida(int id, time_t fechaInicio, float duracion){
+    this->id = id;
+    this->fechaInicio = fechaInicio;
+    this->duracion = duracion;
+}
+
 Partida::~Partida()
 {
 
+}
+
+int Partida::getId() {
+    return this->id;
+}
+
+void Partida::setId(int id) {
+    this->id = id;
+}
+
+time_t Partida::getFechaInicio() {
+    return this->fechaInicio;
+}
+
+void Partida::setFechaInicio(time_t fechaInicio) {
+    this->fechaInicio = fechaInicio;
+}
+
+float Partida::getDuracion() {
+    return this->duracion;
+}
+
+void Partida::setDuracion(float duracion) {
+    this->duracion = duracion;
 }
--- Partida/Partida.h
+++ Partida/Partida.h
@@ -1,5 +1,7 @@
 #ifndef PARTIDA_H
 #define PARTIDA_H
+#include "ctime"
+#include "../Datatypes/dtPartida.h"
 
 #pragma once
 
@@ -7,10 +9,23 @@ class Partida
 {
 public:
     Partida();
+    Partida(int id, time_t fechaInicio, float duracion);
     ~Partida();
+    int getId();
+    void setId(int id);
+    time_t getFechaInicio();
+    void setFechaInicio(time_t fechaInicio);
+    float getDuracion();
+    void setDuracion(float duracion);
+    dtPartida getDtPartida();
+    void calcularDuracion();
+    bool verificarJuego(char* nombreJuego);
+    void eliminarComentarios();
 
 private:
-
+    int id;
+    time_t fechaInicio;
+    float duracion;
 };
 
 #endif
