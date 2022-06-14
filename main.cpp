abbb91038f1ba44eac7e236ece984138690d5ae0
ElPiche <lucasfidel85@gmail.com>
2022-06-14 18:19:09 -0300

    menuJogador

--- main.cpp
+++ main.cpp
@@ -48,3 +48,56 @@ int main(){
     return 0;
 }
 
+int menuJugador(){
+
+    int opcion = 0;
+
+    cout << "=============" << endl;
+    cout << "Menú jugador." << endl;
+    cout << "=============" << endl;
+    
+    cout << "1- Suscribirse a un videojuego" << endl;
+    cout << "2- Asignar puntaje a videojuego" << endl;
+    cout << "3- Iniciar partida" << endl;
+    cout << "4- Abandonar partida multijugador" << endl;
+    cout << "5- Finalizar partida" << endl;
+    cout << "6- Ver información de videojuego" << endl;
+    cout << "7- Modificar fecha del sistema." << endl;
+    cout << "8- Salir" << endl;
+    cin >> opcion;
+    cout << "Presione enter para continuar" << endl;
+    getchar();
+    getchar();
+    system("clear");
+        
+   while (opcion <= 1 || opcion>= 8){
+        try{
+        
+            switch(opcion){
+
+                case 1:
+                    //suscribirseAvideojuego();
+                case 2:
+                    //asignarpuntajeAvideojuego();
+                case 3:
+                    //iniciarPartida
+                case 4:
+                    //AbandonarPartidaMultijugador
+                case 5:
+                    //FinalizarPartida
+                case 6:
+                    //verInformaciondeVideojuego
+                case 7:
+                    //ModificarFechadelSistema
+                case 8:
+                    //menuprincipal();
+                default: 
+                cout << "Opción incorrecta pibe" << endl;
+            }
+        }
+        catch(invalid_argument& error){
+        cout << error.what();
+        }
+    
+   }
+}
