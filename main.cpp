423ec271df2f936daf1ea81630b549d124f80ce1
ShodytheWolf <49129728+ShodytheWolf@users.noreply.github.com>
2022-06-14 23:02:17 -0300

    re incorporación del main del Jona

--- main.cpp
+++ main.cpp
@@ -259,3 +259,41 @@ void menuAsignarPuntaje(){
    }
 
 }
+
+void menuPrincipal(){
+    int opt;
+	do{
+		menuPrincipal();
+        cout<<"Bienvenido a Steam barato!"<<endl;
+	    cout<<"Seleccione una opcion a continuacion:"<<endl;
+	    cout<<"-------------------------------------"<<endl<<endl;
+	    cout<<"1)Alta de Usuario"<<endl;
+	    cout<<"2)Iniciar Sesion"<<endl;
+	    cout<<"3)Cargar datos de prueba"<<endl;
+	    cout<<"4)Salir"<<endl;
+		cin>>opt;
+
+	switch(opt){
+            case 1:
+				//acá se llama a AltaUsuario
+			break;
+
+			case 2:
+				//acá se inicia sesion
+				//se mostrará 2 menus, el de desarrollador o el de jogador, dependiendo quién inicié sesión jaja xd
+			break;
+
+			case 3:
+				//se cargaran datos de prueba
+			break;
+
+			case 4:
+				return;
+			break;
+
+			default:
+			cout<<"opcion invalida"<<endl;
+		}
+	}while(opt != 4);
+
+}
