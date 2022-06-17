9e1b6c49fd6c13f133a52eb146097c475df10ae9
ElPiche <lucasfidel85@gmail.com>
2022-06-17 17:09:13 -0300

    Correciones Icontroler y Controlador

--- Controlador/Controlador.cpp
+++ Controlador/Controlador.cpp
@@ -20,6 +20,105 @@ Controlador* Controlador::getInstance(){
     return instance;
 }
 
-void Controlador::holaMundo(){
-    std::cout << "Holis";
+void Controlador::ingresoDatos(char* email, char* contrasenia){
+    
 }
+
+void Controlador::ingresarEmpresa(char* nombreEmpresa){
+
+}                                       
+void Controlador::confirmarNuevoDesarrollador(){
+
+}                                             
+bool Controlador::ingresarNickname(char* nickname){
+
+}                                           
+void Controlador::ingresarDescripcion(char* descripcion){
+
+}                                     
+void Controlador::confirmarNuevoJugador(){
+
+}                                                    
+bool Controlador::ingresoData(char* email, char* contrasenia){
+
+}                                
+string** Controlador::listarCategoriaXNombre(){
+
+}                                               
+void Controlador::confirmar(dtCategoria datosCategoria){
+
+}                                      
+dtCategoria** Controlador::listarGenero(){
+
+}                                                   
+dtCategoria** Controlador::listarPlataforma(){
+
+}                                                
+dtCategoria** Controlador::listarOtro(){
+
+}                                                      
+void Controlador::publicarVideojuego(dtVideoJuego datosJuegos,dtCategoria**                    
+genero, dtCategoria** plataforma, dtCategoria** otros){
+
+}                            
+dtVideoJuego** Controlador::listarVideojuegosDiferenciada(){
+
+}                                 
+void Controlador::ingresarVideojuego(char* nombreVideojuego){
+
+}                                 
+void Controlador::nuevaSuscripcion(enumPago metodoDePago, enumSuscripcion tipoSuscripcion){
+
+}   
+void Controlador::darDeBajaSuscripcion(){
+
+}                                                   
+dtVideoJuego** Controlador::listarVideojuegos(){
+
+}                                              
+void Controlador::asignarPuntaje(char* nombreVideojuego, int puntuacion){
+
+}                     
+string** Controlador::listarVideojuegosSuscripto(){
+
+}                                           
+dtPartidaIndividual** Controlador::listoPartidasInactivas(){
+
+}                                 
+void Controlador::confirmarIndividual(dtPartidaIndividual datosPartida){
+
+}                      
+string** Controlador::listarNicks(char* nombrejuego){
+
+}                                        
+void Controlador::confirmarMultijugador (dtPartidaMultijugador datosPartida){
+
+}                
+dtPartidaMultijugador** Controlador::listoPartidaMultijugador(){
+
+}                              
+void Controlador::confirmoAbandono(int idPartidaMulti){
+
+}                                       
+dtPartida** Controlador::listoPartidasActivas(){
+
+}                                              
+void Controlador::seleccionarPartida(int idPartida){
+
+}                                         
+dtVideoJuego** Controlador::seleccionarVideojuego(char* nombreVideojuego){
+
+}                    
+string** Controlador::listarVideojuegosPublicados(){
+
+}                                          
+void Controlador::confirmoEliminacion (char* nombreVideojuego){
+
+}                               
+string** Controlador::listaNombresVideojuegos(){
+
+}
+
+string** Controlador::mostrarEstadisticas(char* nombreJuego,string** opciones){
+
+}           
--- IControlador/IControlador.h
+++ IControlador/IControlador.h
@@ -5,55 +5,53 @@
 #include "../Datatypes/dtPartidaMultijugador.h"
 #include "../Enumeration/enumSuscripcion.h"
 #include "../Enumeration/enumPago.h"
+#include "../Datatypes/dtVideoJuego.h"
+#include "../Datatypes/dtJugador.h"
+#include <iostream>
 
-
+using namespace std;
 class IControlador{
 
     private: 
 
     public:
         virtual ~IControlador();
-        virtual void holaMundo() = 0;
-        // virtual void ingresoDatos(char* email, char* contrasenia) = 0;              //1
-        // virtual void ingresarEmpresa(char* nombreEmpresa) = 0;                      //2
-        // virtual void confirmarNuevoDesarrollador() = 0;                             //3
-        // virtual bool ingresarNickname(char* nickname) = 0;                          //4
-        // virtual void ingresarDescripcion(char* descripcion) = 0;                    //5
-        // virtual void confirmarNuevoJugador() = 0;                                   //6
-        // virtual bool ingresoData(char* email, char* contrasenia) = 0;              //7
-        // virtual void listarCategoriaXNombre() = 0; //SET <string>                   //8
-        // virtual void confirmar(dtCategoria datosCategoria) =  0;                    //9
-        // virtual void listarGenero() =  0; //SET <dtCategoria>                       //10
-        // virtual void listarPlataforma() =  0; //SET <dtCategoria>                   //11
-        // virtual void listarOtro() =  0; //SET <dtCategoria>                         //12
-        // //virtual void publicarVideojuego(dtVideojuego datosJuegos,SET <dtGenero>   //13
-        // //genero, SET<dtCategoria> plataforma, SET <dtCategoria> otros)
-        // virtual void listarVideojuegosDiferenciada() = 0; //SET <dtVideojuego>                  //14
-        // virtual void ingresarVideojuego(char* nombreVideojuego) = 0;                //15
-        // virtual void nuevaSuscripcion(enumPago metodoDePago, enumSuscripcion tipoSuscripcion) = 0; //16
-        // virtual void darDeBajaSuscripcion() = 0;                                    //17
-        // virtual void listarVideojuegos() = 0; //SET <dtVideojuego>                  //18
-        // virtual void asignarPuntaje(char* nombreVideojuego, int puntuacion) = 0;    //19
-        // virtual void listarVideojuegosSuscripto() = 0; //SET <string>               //20
-        // virtual void listoPartidasInactivas() = 0; //SET <dtPartidaIndividual>      //21
-        // virtual void confirmarIndividual(dtPartidaIndividual datosPartida) = 0;     //22
-        // //virtual SET<string> listarNicks(char* nombrejuego) = 0;                   //23
-        // virtual void confirmarMultijugador (dtPartidaMultijugador datosPartida) = 0;//24
-        // //virtual SET<dtPartidaMultijugador> listoPartidaMultijugador() = 0;        //25
-        // virtual void confirmoAbandono(int idPartidaMulti) = 0;                      //26
-        // //virtual SET<dtPartida> listoPartidasActivas() = 0;                        //27
-        // virtual void seleccionarPartida(int idPartida) = 0;                         //28
-        // virtual void enviaComentario(char* comentario) = 0;                         //29
-        // //virtual void SET <dtComentario> listarComentariosExistentes() = 0;          //30
-        // virtual void enviaComentario(int id, char* comentario) = 0;                         //31
-        // //virtual SET<dtVideojuego> seleccionarVideojuego(char* nombreVideojuego)=0; //32
-        // //virtual SET <dtJugador> listarJugadores() = 0;                            //33
-        // virtual void seleccionarJugador(char* nickname) = 0;                        //34
-        // //virtual SET<string> listarVdeiojuegosPublicados() = 0;                    //35
-        // virtual void confirmoEliminacion (char* nombreVideojuego) = 0;              //36
-        // //virtual SET<string> listarVideojuegos() = 0;                              //37
-        // //virtual SET<dtEstadistica> mostrarEstadisticas(char* nombreJuego,
-        // //SET<string> opciones) = 0;                                                //38
+
+        virtual void ingresoDatos(char* email, char* contrasenia) = 0;                                //1
+        virtual void ingresarEmpresa(char* nombreEmpresa) = 0;                                        //2
+        virtual void confirmarNuevoDesarrollador() = 0;                                               //3
+        virtual bool ingresarNickname(char* nickname) = 0;                                            //4
+        virtual void ingresarDescripcion(char* descripcion) = 0;                                      //5
+        virtual void confirmarNuevoJugador() = 0;                                                     //6
+        virtual bool ingresoData(char* email, char* contrasenia) = 0;                                 //7
+        virtual string** listarCategoriaXNombre() = 0;                                                //8
+        virtual void confirmar(dtCategoria datosCategoria) = 0;                                       //9
+        virtual dtCategoria** listarGenero() = 0;                                                         //10
+        virtual dtCategoria** listarPlataforma() = 0;                                                     //11
+        virtual dtCategoria** listarOtro() = 0;                                                           //12
+        virtual void publicarVideojuego(dtVideoJuego datosJuegos,dtCategoria**                          //13
+        genero, dtCategoria** plataforma, dtCategoria** otros) = 0;
+        virtual dtVideoJuego** listarVideojuegosDiferenciada() = 0;                                   //14
+        virtual void ingresarVideojuego(char* nombreVideojuego) = 0;                                  //15
+        virtual void nuevaSuscripcion(enumPago metodoDePago, enumSuscripcion tipoSuscripcion) = 0;    //16
+        virtual void darDeBajaSuscripcion() = 0;                                                      //17
+        virtual dtVideoJuego** listarVideojuegos() = 0;                                               //18
+        virtual void asignarPuntaje(char* nombreVideojuego, int puntuacion) = 0;                      //19
+        virtual string** listarVideojuegosSuscripto() = 0;                                            //20
+        virtual dtPartidaIndividual** listoPartidasInactivas() = 0;                                   //21
+        virtual void confirmarIndividual(dtPartidaIndividual datosPartida) = 0;                       //22
+        virtual string** listarNicks(char* nombrejuego) = 0;                                          //23
+        virtual void confirmarMultijugador (dtPartidaMultijugador datosPartida) = 0;                  //24
+        virtual dtPartidaMultijugador** listoPartidaMultijugador() = 0;                               //25
+        virtual void confirmoAbandono(int idPartidaMulti) = 0;                                        //26
+        virtual dtPartida** listoPartidasActivas() = 0;                                               //27
+        virtual void seleccionarPartida(int idPartida) = 0;                                           //28
+        virtual dtVideoJuego** seleccionarVideojuego(char* nombreVideojuego) = 0;                     //29
+        virtual string** listarVideojuegosPublicados() = 0;                                           //30
+        virtual void confirmoEliminacion (char* nombreVideojuego) = 0;                                //31
+        virtual string** listaNombresVideojuegos() = 0;                                               //32                                                     //32
+        virtual string** mostrarEstadisticas(char* nombreJuego,string** opciones)  = 0;               //33
+                                                                       
         
 };
 
