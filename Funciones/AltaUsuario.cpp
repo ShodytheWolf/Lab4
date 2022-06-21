360af70f63265d0b52f3cc2388a6ea39376437e4
Braian <braianbarrios_29@hotmail.com>
2022-06-21 15:39:48 -0300

    makefile andando no lo rompan hdps

--- Funciones/AltaUsuario.cpp
+++ Funciones/AltaUsuario.cpp
@@ -30,7 +30,7 @@ void altaUsuario(){
     cout << "\nIngrese una contrasenia: ";
     cin >> contrasenia;
 
-    controlador->ingresoDatos(correo, contrasenia);
+    //controlador->ingresoDatos(correo, contrasenia);
 
     cout << "¿Desea registrar un desarrolladoro un usuario?" << endl;
     cout << "Para registrar un desarrollador presione(d/D)" << endl;
--- Funciones/PublicarVideojuego.cpp
+++ Funciones/PublicarVideojuego.cpp
@@ -8,58 +8,51 @@ using namespace std;
 
 void publicarVideojuego(){
 
-Fabrica fab;
-IControlador* controlador = fab.getInterface();
+    Fabrica fab;
+    IControlador* controlador = fab.getInterface();
 
-dtCategoria** generos;
-dtCategoria** plataformas;
-dtCategoria** otros;
+    dtCategoria** generos;
+    dtCategoria** plataformas;
+    dtCategoria** otros;
 
 
 
-int opcion = 0;
-char confirmar;
-do{
-    cout << "|=======================|" << endl;
-    cout << " Publicar un videojuego." << endl;
-    cout << "|=======================|" << endl;
+    int opcion = 0;
+    char confirmar;
+    do{
+        cout << "|=======================|" << endl;
+        cout << " Publicar un videojuego." << endl;
+        cout << "|=======================|" << endl;
 
-    cout << "1- Listar plataforma." << endl;
-    cout << "2- Listar género." << endl;
-    cout << "3- Listar otro." << endl;
-    cout << "4- Publicar videojuego." << endl;
-    cout << "5- Menu principal." << endl;
-    cin >> opcion;
-    system("clear");
+        cout << "1- Listar plataforma." << endl;
+        cout << "2- Listar género." << endl;
+        cout << "3- Listar otro." << endl;
+        cout << "4- Publicar videojuego." << endl;
+        cout << "5- Menu principal." << endl;
+        cin >> opcion;
+        system("clear");
 
-    try{
-        switch(opcion){
-
-            case 1:
+        try{
                 cout << "Entraste a listar y seleccionar genero" << endl;
                 cout << "Ingrese el genero a seleccionar: " << endl;
 
-                dtCategoria** generos = controlador->listarGenero(); //violacion de segmento (core)
+                dtCategoria** listGeneros = controlador->listarGenero(); //violacion de segmento (core)
                 //
                 //listarGenero();
                 //mostrar
                 //seleccion
-                break;
 
-            case 2:
                 cout << "Entraste a listar y seleccionar plataforma" << endl;
-                dtCategoria** plataformas = controlador->listarPlataforma();
+                dtCategoria** listPlataformas = controlador->listarPlataforma();
                 //listarplataforma();
                 //mostrar
                 //seleccion
-                break;
 
-            case 3:
                 //listar
                 //mostrar
                 //selecc
                 cout << "Entraste a listar Otros" << endl;
-                dtCategoria** otros = controlador->listarOtro();
+                dtCategoria** listOtros = controlador->listarOtro();
 
                 cout << "¿Desea seleccionar otros?" << endl;
                 cout << "Presione (s/S) para confirmar o cualquier tecla para cancelar" << endl;
@@ -70,9 +63,7 @@ do{
                 else {
                     //menuDesarrollador();
                 }
-                break;
 
-            case 4:
                 cout << "Confirme si desea publicar un videojuego:" << endl;
                 cout << "Presione (s/S) para confirmar o cualquier tecla para cancelar" << endl;
                 cin >> confirmar;
@@ -83,25 +74,18 @@ do{
                 else {
                     cout << "cancelaste" << endl;
                 }
-                break;
 
-            case 5:
                 cout << "Entraste a menuprincipal" << endl;
                 //menuPrincipal;
-            break;
 
-            default: 
             cout << "Opción incorrecta. Presione ENTER tecla para continuar nuevamente." << endl;
             getchar();
             getchar();
             system("clear");
-            break;
         }
-    }
 
-    catch(invalid_argument& error){
-    cout << error.what();
-    }
-
-} while(opcion >= 1 || opcion <= 5);
+        catch(invalid_argument& error){
+        cout << error.what();
+        }
+    } while(opcion >= 1 || opcion <= 5);
 }
