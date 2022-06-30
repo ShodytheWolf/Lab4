ba5ca5f8c4ae22da09214a053a325a8caf6fc4b9
Braian <braianbarrios_29@hotmail.com>
2022-06-30 14:36:18 -0300

    Jugador update

--- Jugador/Jugador.cpp
+++ Jugador/Jugador.cpp
@@ -105,18 +105,14 @@ void Jugador::iniciarIndividual(dtPartidaIndividual* datosPartida,Videojuego* vj
         Individual* partiAAnadiar = new Individual(idUltimaPartida,dameLaHora,horadiferida,vj,partiContinuada);
         delete k;
 
-        OrderedKey* k = new Integer(idUltimaPartida);
-
-        this->partidasActivas->add(k,partiAAnadiar);
+        this->partidasActivas->add(new Integer(idUltimaPartida),partiAAnadiar);
     }else{
 
         //OrderedKey* k = new Integer(datosPartida->getIdPartidaAnterior());//conseguimos la key
         time_t dameLaHora = time(NULL);
         Individual* partiAAnadiar = new Individual(idUltimaPartida,dameLaHora,0,vj,NULL);
 
-        OrderedKey* k = new Integer(idUltimaPartida);
-
-        this->partidasActivas->add(k,partiAAnadiar);
+        this->partidasActivas->add(new Integer(idUltimaPartida),partiAAnadiar);
     }
     return;
 }
