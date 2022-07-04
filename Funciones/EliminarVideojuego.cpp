0cb8f342254e6775d1e0ef607ca80b4a9bf37e72
Braian <braianbarrios_29@hotmail.com>
2022-07-04 15:22:49 -0300

    update EliminarVideojuego

--- Funciones/EliminarVideojuego.cpp
+++ Funciones/EliminarVideojuego.cpp
@@ -10,44 +10,48 @@ bool verificarEnLista(string**, string);
 void eliminarVideojuego(){
     Fabrica f;
     IControlador* sistema = f.getInterface();
-    char* juego;
+    string juego;
     char op;
     bool loop;
-    string** listVJ = sistema->listarVideojuegosPublicados();
-    if(!listVJ[0])
-        throw invalid_argument("El desarrollador no ha publicado ningun videojuego");
-    cout << "Por favor elija un videojuego ingresando su nombre: " <<endl;
-    int i = 0;
-    while(listVJ[i]){
-        cout <<"-"<< *listVJ[i] <<endl;
-        i++; 
-    }
-    cin >> juego;
-    while(!verificarEnLista(listVJ, string(juego))){
-        cout << "El juego no existe. Por favor intentelo de nuevo" <<endl;
-        cin >> juego; 
-    }
-    cout << "Se eliminara el Videojuego: " << juego <<"\nDesea confirmar? S/N\n" <<endl;
-    do{
-        cin >> op;
-        switch (op)
-        {
-        case 's':
-        case 'S':
-            sistema->confirmoEliminacion(juego);
-            loop = false;
-            break;
-
-        case 'n':
-        case 'N':
-            cout << "Cancelando eliminacion..." <<endl;
-            loop = false;
-
-        default:
-            cout << "Opcion incorrecta. Intente nuevamente" <<endl;
-            loop = true; 
-            break;
+    try{
+        string** listVJ = sistema->listarVideojuegosPublicados();
+        cout << "Por favor elija un videojuego ingresando su nombre: " <<endl;
+        int i = 0;
+        while(listVJ[i]){
+            cout <<"-"<< *listVJ[i] <<endl;
+            i++; 
         }
-    }while(loop);
-    delete juego, listVJ;
+        getline(cin.ignore(),juego,'\n');
+        while(!verificarEnLista(listVJ, juego)){
+            cout << "El juego no existe. Por favor intentelo de nuevo" <<endl;
+            cin >> juego; 
+        }
+        cout << "Se eliminara el Videojuego: " << juego <<"\nDesea confirmar? S/N\n" <<endl;
+        do{
+            cin >> op;
+            switch (op)
+            {
+            case 's':
+            case 'S':
+                sistema->confirmoEliminacion(juego.data());
+                cout << "Se elimino correctamente " << juego <<"\n\nPresione <enter> para continuar" << endl;
+                getchar();
+                loop = false;
+                break;
+            case 'n':
+            case 'N':
+                cout << "Cancelando eliminacion..." <<endl;
+                loop = false;
+                break;
+
+            default:
+                cout << "Opcion incorrecta. Intente nuevamente" <<endl;
+                loop = true; 
+                break;
+            }
+        }while(loop);
+    }catch(invalid_argument &e){
+        cout << e.what() << " Presione <enter> para continuar"<<endl;
+        getchar();
+    }
 }
